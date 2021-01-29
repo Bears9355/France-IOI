8b3b11712779ce6348ffc305d13f6c5a0c9edaa4
Bears9355 <52669057+Bears9355@users.noreply.github.com>
2021-01-29 15:45:55 +0100

    Changes - Less lines
    Delete useless lines (2 print lines)

--- Lvl-1/02-Répétitions d'instructions/01-Jeu de dames.py
+++ Lvl-1/02-Répétitions d'instructions/01-Jeu de dames.py
@@ -1,9 +1,23 @@
-for loop in range(20) :
-   for loop in range(20) :
-      print("O",end="")
-      print("X",end="")
+#for loop in range(20) :
+#   for loop in range(20) :
+#      print("O",end="")
+#      print("X",end="")
+#   print()
+#   for loop in range(20) :
+#      print("X",end="")
+#      print("O",end="")
+#   print()
+
+
+# Nous pouvons supprimer deux lignes inutiles en faisant ceci :
+# We could delete useless lines like this :
+
+for loop in range(20):
+   for loop in range(20):
+      print("OX", end = "")
    print()
-   for loop in range(20) :
-      print("X",end="")
-      print("O",end="")
+   for loop in range(20):
+      print("XO", end = "")
    print()
+
+   
